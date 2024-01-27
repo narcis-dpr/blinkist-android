d66845d17a7cd3801db3de88b9dbd00f6c0d8350
n.dipir <nargesdeypir@gmail.com>
2024-01-27 17:13:27 +0330

    data module -- setup test for book api

--- data/src/test/java/com/blinkist/data/remote/BooksApiTest.kt
+++ data/src/test/java/com/blinkist/data/remote/BooksApiTest.kt
@@ -1,24 +1,25 @@
-package com.blinkist.data
+package com.blinkist.data.remote
 
-import com.blinkist.data.remote.BooksApi
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
-import org.junit.Before
+import org.junit.Rule
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class BooksApiTest {
-    private lateinit var server: MockWebServer
+
+    @get:Rule
+    val server = MockWebServer()
     private lateinit var api: BooksApi
 
-    @Before
-    fun beforeEach() {
-        server = MockWebServer()
-        api = Retrofit.Builder()
-            .baseUrl(server.url(""))
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(server.url("/"))
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(BooksApi::class.java)
+    }
+    private val bookService by lazy {
+        retrofit.create(BooksApi::class.java)
     }
 
     @After
